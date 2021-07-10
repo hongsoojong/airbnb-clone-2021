00bb7aa0808aedc1dc277766e95dadb429a9414a
hongsoojong <84722193+hongsoojong@users.noreply.github.com>
2021-07-10 20:43:57 +0900

    setting

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hong soo jong\desktop\airbnb-clone-2021\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
