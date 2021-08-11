f8cd505f36b36568971b60bc6823bae54caafd4f
hongsoojong <84722193+hongsoojong@users.noreply.github.com>
2021-08-11 16:23:10 +0900

    Updated user login and logout -2

--- users/forms.py
+++ users/forms.py
@@ -2,27 +2,20 @@ from django import forms
 from django.core import exceptions
 from . import models
 
+
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
-        email = self.cleaned_data("email")
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
         try:
-            models.User.objects.get(username=email)
-            return email
-        except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
-
-    def clean_password(self):
-        email = self.cleaned_data("email")
-        password = self.cleaned_data("password")
-        try:
-            user = models.User.objects.get(username=email)
+            user = models.User.objects.get(email=email)
             if user.check_password(password):
-                return password
-            else forms.exceptions.ValidationError("Password is wrong")
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
-            pass
-        
+            self.add_error("email", forms.ValidationError("User does not exist"))
