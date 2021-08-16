1e9cb9730983981bc7dd3cbfe430c570700f167f
hongsoojong <84722193+hongsoojong@users.noreply.github.com>
2021-08-16 14:45:04 +0900

    Upated make it all beautiful

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from django.core import exceptions
+from django.forms import widgets
 from . import models
 
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -25,9 +28,19 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = models.User
         fields = ("first_name", "last_name", "email")
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last name"}),
+            "email": forms.TextInput(attrs={"placeholder": "Email"}),
+        }
 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}),
+        label="Confirm Password",
+    )
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
--- users/views.py
+++ users/views.py
@@ -34,11 +34,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "Soojong",
-        "last_name": "Hong",
-        "email": "hongsoojong@naver.com",
-    }
 
     def form_valid(self, form):
         form.save()
