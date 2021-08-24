8c2dda122540e15c133d863535858ee348ae6e46
hongsoojong <84722193+hongsoojong@users.noreply.github.com>
2021-08-24 11:48:57 +0900

    kakao-1

--- users/views.py
+++ users/views.py
@@ -142,7 +142,7 @@ def kakao_callback(request):
     try:
         code = request.GET.get("code")
         REST_API_KEY = os.environ.get("KAKAO_ID")
-        REDIRECT_URI = "http://127.0.0.1:8000/users/login/kakao/callback"
+        REDIRECT_URI = "http://airbnb-clone.eba-jdcipkvv.ap-northeast-2.elasticbeanstalk.com/users/login/kakao/callback"
         token_request = requests.get(
             f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&code={code}"
         )
